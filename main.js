6138a9351a7227543e6f3e90483168307583093f
Your Name <youamariappan2001@example.com>
2023-03-24 16:35:02 +0530

    add

---
+++ main.js
@@ -0,0 +1,48 @@
+//using method-3
+	const users=[
+	{
+		first_name:"mari",
+	location:"london"
+	},
+	{
+		first_name:"sri",
+	location:"us"
+	},
+	{
+		first_name:"sathish",
+	location:"australia"
+	},
+	];
+	
+	var a=users.map(function(val){
+		return val.first_name+" lives in  "+val.location;
+	})
+	console.log(a);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	
+	
+	
